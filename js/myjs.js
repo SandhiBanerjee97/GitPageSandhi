c4f0e2b4ae5d6da34906aecc134df1b845719961
sandhi <sandhi.banerjee.97@gmail.com>
2021-02-20 00:16:23 +0530

    initial commit

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){           // When document is ready
+    $('.sidenav').sidenav();            //nav-bar initializes
+    $('.parallax').parallax();          //parallax initilaizes
+ });
+ $(document).ready(function(){          // cossapsable initializes
+    $('.collapsible').collapsible({accordion: false});
+  });
